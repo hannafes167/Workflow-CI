e6eebd42742478fb4698c0fa0a01696946978e1c
hannafes167 <147309234+hannafes167@users.noreply.github.com>
2025-06-20 10:20:53 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+import warnings
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-X_train = pd.read_csv("X_train.csv").astype('float64')
-X_test = pd.read_csv("X_test.csv").astype('float64')
-y_train = pd.read_csv("y_train.csv").values.ravel()
-y_test = pd.read_csv("y_test.csv").values.ravel()
+warnings.filterwarnings("ignore")
+
+X_train = pd.read_csv("heart-disease_preprocessing/X_train.csv")
+X_test = pd.read_csv("heart-disease_preprocessing/X_test.csv")
+y_train = pd.read_csv("heart-disease_preprocessing/y_train.csv").values.ravel()
+y_test = pd.read_csv("heart-disease_preprocessing/y_test.csv").values.ravel()
 
 mlflow.set_tracking_uri("file:./mlruns")  
 mlflow.set_experiment("HeartDisease_Basic")
-
 mlflow.sklearn.autolog()
 
 with mlflow.start_run(run_name="basic_autolog_run"):
@@ -19,10 +21,11 @@ with mlflow.start_run(run_name="basic_autolog_run"):
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
 
-  
     mlflow.log_metric("test_accuracy", accuracy_score(y_test, y_pred))
     mlflow.log_metric("test_precision", precision_score(y_test, y_pred))
     mlflow.log_metric("test_recall", recall_score(y_test, y_pred))
     mlflow.log_metric("test_f1", f1_score(y_test, y_pred))
 
     print("Training dan testing selesai.")
+
+mlflow.end_run()
