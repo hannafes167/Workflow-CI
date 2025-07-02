18ac25ba97cdd2118ccb86b9c0d53b80f9078c7b
hannafes167 <147309234+hannafes167@users.noreply.github.com>
2025-07-02 16:10:42 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,29 +1,27 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-import warnings
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-warnings.filterwarnings("ignore")
-
-X_train = pd.read_csv("heart-disease_preprocessing/X_train.csv")
-X_test = pd.read_csv("heart-disease_preprocessing/X_test.csv")
+X_train = pd.read_csv("heart-disease_preprocessing/X_train.csv").astype('float64')
+X_test = pd.read_csv("heart-disease_preprocessing/X_test.csv").astype('float64')
 y_train = pd.read_csv("heart-disease_preprocessing/y_train.csv").values.ravel()
 y_test = pd.read_csv("heart-disease_preprocessing/y_test.csv").values.ravel()
 
-mlflow.set_tracking_uri("http://127.0.0.1:5000")  
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("HeartDisease_Basic")
+
 mlflow.sklearn.autolog()
 
-# Tanpa mlflow.start_run(), karena run sudah otomatis dimulai oleh CLI
-model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+with mlflow.start_run(run_name="basic_autolog_run"):
+    model = RandomForestClassifier(n_estimators=100, max_depth=10, random_state=42)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
 
-mlflow.log_metric("test_accuracy", accuracy_score(y_test, y_pred))
-mlflow.log_metric("test_precision", precision_score(y_test, y_pred))
-mlflow.log_metric("test_recall", recall_score(y_test, y_pred))
-mlflow.log_metric("test_f1", f1_score(y_test, y_pred))
+    mlflow.log_metric("test_accuracy", accuracy_score(y_test, y_pred))
+    mlflow.log_metric("test_precision", precision_score(y_test, y_pred))
+    mlflow.log_metric("test_recall", recall_score(y_test, y_pred))
+    mlflow.log_metric("test_f1", f1_score(y_test, y_pred))
 
-print("Training dan testing selesai.")
+    print("Training dan testing selesai.")
